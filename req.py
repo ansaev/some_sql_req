e2681942d88cc49a65e0e2f7c6bfaa91b6a036d0
Anatolii Saevskii <ansaev@gmail.com>
2016-02-08 16:57:50 +0300

    added heddings to columns in request sql

--- req.py
+++ req.py
@@ -1,3 +1,5 @@
-sql_request = "SELECT `users`.`Name`, count(`messages`.`msg`) FROM `users` " \
+# -*- coding: utf-8 -*-
+sql_request = "SELECT `users`.`Name` AS 'Имя пользователя', count(`messages`.`msg`) AS 'Общее количество сообщений' " \
+              "FROM `users` " \
               "LEFT JOIN `messages` ON `messages`.`UID` == `users`.`UID` " \
               "GROUP BY `messages`.`UID`;"
--- tests.py
+++ tests.py
@@ -9,6 +9,5 @@ sys.setdefaultencoding('utf-8')
 def test_request():
     conn = sqlite3.connect('test.db')
     rez = conn.execute(sql_request).fetchall()
-    print(rez)
     conn.close()
     assert rez == [("Лера Страза", 0), ("Платон Щукин", 2), ("Георгий Атласов", 2)]
