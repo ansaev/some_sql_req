d7a78c30559ec849647169a79f70c1dfd50a9d58
Anatolii Saevskii <ansaev@gmail.com>
2016-02-08 16:50:50 +0300

    made request and it passed the test

--- req.py
+++ req.py
@@ -1,1 +1,3 @@
-sql_request = "SELECT * FROM `users`;"
+sql_request = "SELECT `users`.`Name`, count(`messages`.`msg`) FROM `users` " \
+              "LEFT JOIN `messages` ON `messages`.`UID` == `users`.`UID` " \
+              "GROUP BY `messages`.`UID`;"
--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
-import sys
 # -*- coding: utf-8 -*-
+import sys
 from req import sql_request
 import sqlite3
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def test_request():
     conn = sqlite3.connect('test.db')
     rez = conn.execute(sql_request).fetchall()
-    # rez = conn.fetchall()
     print(rez)
     conn.close()
-    assert rez == [("Платон Щукин", 2), ("Георгий Атласов", 1)]
+    assert rez == [("Лера Страза", 0), ("Платон Щукин", 2), ("Георгий Атласов", 2)]
